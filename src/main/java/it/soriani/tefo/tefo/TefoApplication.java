813923848135f3fcf02dc0b9edc66e32fe334ba8
christiansoriani <issuper190698>
2023-09-25 22:34:27 +0200

    first commit

---
+++ src/main/java/it/soriani/tefo/tefo/TefoApplication.java
@@ -0,0 +1,13 @@
+package it.soriani.tefo.tefo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TefoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TefoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/it/soriani/tefo/tefo/TefoApplicationTests.java
@@ -0,0 +1,13 @@
+package it.soriani.tefo.tefo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TefoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
